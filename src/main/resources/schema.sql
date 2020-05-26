cf8e2df3851c74027c8d13d94426425fdef6c86c
Prashant Swami <prashant.swami@robustwealth.com>
2020-05-26 13:44:02 +0530

    Updated sql changed database name

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,14 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 06, 2020 at 09:25 AM
+-- Generation Time: May 26, 2020 at 10:10 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.1.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";git add README.md
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -19,8 +19,47 @@ SET time_zone = "+00:00";git add README.md
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `hello`
+-- Database: `Technical Thursday`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hobby`
+--
+
+CREATE TABLE `hobby` (
+  `id` int(11) NOT NULL,
+  `hobby_name` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `hobby`
+--
+
+INSERT INTO `hobby` (`id`, `hobby_name`) VALUES
+(1, 'Reading'),
+(2, 'Cricket');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Moderator '),
+(4, 'Guest');
 
 -- --------------------------------------------------------
 
@@ -28,37 +67,70 @@ SET time_zone = "+00:00";git add README.md
 -- Table structure for table `todos`
 --
 
-create TABLE `todos` (
+CREATE TABLE `todos` (
   `id` int(11) NOT NULL,
   `message` varchar(40) NOT NULL,
   `done` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON update current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `users`
 --
 
-create TABLE `user` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `first_name` varchar(40) NOT NULL,
-  `last_name` varchar(40) NOT NULL
+  `last_name` varchar(40) NOT NULL,
+  `role_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `users`
 --
 
-insert into `user` (`id`, `first_name`, `last_name`) values
-(2, 'Amberish', 'Raj'),
-(3, 'Suneet', 'Flash');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `role_id`) VALUES
+(1, 'prashant', 'Swami', 1),
+(2, 'Amberish', 'Raj', 1),
+(3, 'Suneet', 'Flash', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_hobby`
+--
+
+CREATE TABLE `user_hobby` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `hobby_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_hobby`
+--
+
+INSERT INTO `user_hobby` (`id`, `user_id`, `hobby_id`) VALUES
+(1, 1, 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `hobby`
+--
+ALTER TABLE `hobby`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `todos`
@@ -67,14 +139,34 @@ ALTER TABLE `todos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `users`
 --
-ALTER TABLE `user`
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_role_id` (`role_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `user_hobby`
+--
+ALTER TABLE `user_hobby`
   ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `hobby`
+--
+ALTER TABLE `hobby`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `todos`
@@ -83,37 +175,18 @@ ALTER TABLE `todos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_hobby`
+--
+ALTER TABLE `user_hobby`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-- --------------------------------------------------------
-
---
--- Table structure for table `hobby`
---
-
-create TABLE `hobby` (
-  `id` int(11) NOT NULL,
-  `hobby_name` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-ALTER TABLE `hobby`
-  ADD PRIMARY KEY (`id`);
-
-  --
--- Table structure for table `user_hobby`
---
-
-  create TABLE `user_hobby` (
-  `user_id` int(11) NOT NULL,
-  `hobby_id` int(11)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
