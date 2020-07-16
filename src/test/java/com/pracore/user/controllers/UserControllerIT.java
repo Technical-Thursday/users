ee62919380fbd1457f616e3516ddef548e0538f3
Prashant Swami <prashant.swami@robustwealth.com>
2020-07-16 12:47:11 +0530

    unit testing try - 1

---
+++ src/test/java/com/pracore/user/controllers/UserControllerIT.java
@@ -0,0 +1,16 @@
+package com.pracore.user.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserController.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerIT {
+	
+	@Test
+	public void contextLoads() {
+		assert(true);
+	}
+}
