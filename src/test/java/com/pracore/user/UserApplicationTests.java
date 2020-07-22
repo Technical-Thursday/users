a69f2125afcbe2189661f1a73a6944d23155deaa
Suneet <suneet-bendre@github.com>
2020-07-22 11:57:52 +0530

    test application runner

--- src/test/java/com/pracore/user/UserApplicationTests.java
+++ src/test/java/com/pracore/user/UserApplicationTests.java
@@ -1,9 +1,12 @@
 package com.pracore.user;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest()
 class UserApplicationTests {
 
 	@Test
