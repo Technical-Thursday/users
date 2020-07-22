e3bdf1eda533750f202f2500b4a74be83e2db964
Suneet <suneet-bendre@github.com>
2020-07-22 12:36:26 +0530

    user Test configuration

--- src/main/java/com/pracore/user/UserApplication.java
+++ src/main/java/com/pracore/user/UserApplication.java
@@ -1,6 +1,7 @@
 package com.pracore.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @ComponentScan
 @SpringBootApplication
+@EnableAutoConfiguration
 public class UserApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/pracore/user/controllers/UserControllerIT.java
+++ src/test/java/com/pracore/user/controllers/UserControllerIT.java
@@ -1,16 +1,28 @@
 package com.pracore.user.controllers;
 
+import com.pracore.user.UserApplication;
+import com.pracore.user.models.User;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = UserController.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = UserApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class UserControllerIT {
-	
-	@Test
-	public void contextLoads() {
-		assert(true);
-	}
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() {
+        List<User> response = (List<User>) restTemplate.getForEntity("http://localhost:8080", List.class);
+        Assert.assertTrue(response != null);
+        assert (true);
+    }
 }
